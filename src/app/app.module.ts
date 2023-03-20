bd779e184014870472f46c2c4eef63df8fd813c2
Kirthi Vignesh Govindarjan <kirthivignesh.kvg@protonmail.com>
2023-03-20 18:53:21 +0530

    Added Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatCardModule,} from '@angular/material/card'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { MaterialModule } from './modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import { HomePageComponent } from './home-page/home-page.component'; 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+// export class LoginComponent {
 
+// }
+
+
+export class LoginComponent {
+  loginFormEmail: FormGroup;
+  constructor(     private router: Router,) {
+    this.loginFormEmail = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required,Validators.minLength(8)])
+    });
+  }
+  hide = true;
+
+  onSubmit(){
+    this.router.navigate(['/home']);
+  }
+  getErrorMessage() {
+    if (this.loginFormEmail.get('email')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.loginFormEmail.get('email')?.hasError('email') ? 'Not a valid email':" ";
+  }
+  getErrorMessagePassword(){
+    return this.loginFormEmail.get('password')?.hasError('minLength') ? "Minimum length of password is 8":"Minimum length of password is 8";
+
+  }
 }
