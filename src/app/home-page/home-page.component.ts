a3d5829eee6046af409f651f54c0528dbe5e9109
KirthiVignesh <kirthivignesh.kvg@protonmail.com>
2023-03-21 20:43:19 +0530

    Fixed Weather data retreival

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 
 @Component({
@@ -11,15 +12,17 @@ import { HttpClient } from '@angular/common/http';
 export class HomePageComponent {
 
   constructor(private http: HttpClient) { }
-
+Tempdata:any
 
   getLocation(): void{
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position)=>{
           const longitude = position.coords.longitude;
           const latitude = position.coords.latitude;
-          const res = this.getWeather(longitude, latitude);
-          console.log(res.forEach((val)=>{console.log(val)}))
+            // console.log(latitude,longitude)
+          this.getWeather(longitude, latitude);
+          // res.forEach((val)=>{console.log(val)})
+          console.log(this.Tempdata)
         });
     } else {
        console.log("No support for geolocation")
@@ -27,6 +30,17 @@ export class HomePageComponent {
   }
 
   getWeather(longitude:Number,latitude:Number){
-    return this.http.get("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&appid=0f6f3dee7ed9462c6abc4e91d68a6082")
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&appid=0f6f3dee7ed9462c6abc4e91d68a6082")    .then(response=>response.json()).then(data=>{this.addTempData(data)})
+ 
   }
+  addTempData(data:any){this.Tempdata = data;
+     this.Tempdata.temp = this.Tempdata.main.temp 
+     this.Tempdata.temp_like = this.Tempdata.main.feels_like
+     this.Tempdata.max = this.Tempdata.main.temp_max
+     this.Tempdata.min = this.Tempdata.main.temp_min
+     this.Tempdata.location=this.Tempdata.name
+    this.Tempdata.desc=this.Tempdata.weather[0].main
+    this.Tempdata.humidity=this.Tempdata.main.humidity
+  }
+
   }
